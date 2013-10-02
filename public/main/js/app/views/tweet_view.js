2f9f5ccb65d3f6004750b861e4d564c9f8d7280f
Juan David Millan <juanmillan85@gmail.com>
2013-10-02 14:23:59 +0100

    Adding tweet.hbs, tweet_view.js

---
+++ public/main/js/app/views/tweet_view.js
@@ -0,0 +1,25 @@
+App.TweetView = Ember.View.extend({
+    tagName: 'div',
+    classNames: ['thumbnail section'],
+   // classNameBindings: 'isSelected',
+    templateName: 'tweet',
+
+    /*isSelected: function() {
+        return this.get('content.id') === this.get('controller.controllers.photosSelectedPhoto.content.id');
+    }.property('controller.controllers.photosSelectedPhoto.content', 'content')
+*/
+
+	tweetUrl: function() {
+			
+			var tweet =this.get('content.tweet');
+	     	var user = tweet.user.screen_name;
+	        return 'http://twitter.com/'+user;
+	       // return 'http://twitter.com/#!/%@/status/%@'.fmt(user, id);
+	}.property(),
+    tweetProfileImage: function() {
+		var tweet =this.get('content.tweet');
+   		var profile_image = tweet.user.profile_image_url_https;
+   		return profile_image;
+   }.property()
+
+});
---
+++ public/main/js/libs/handlebars-helpers.js
@@ -0,0 +1,26 @@
+
+/*
+Ember.Handlebars.registerHelper('parseTweet',
+function(propertyPath, options) {
+    var tweet;
+    if (!options) {
+        options = propertyPath;
+        tweet = options.contexts[0].text;
+    } else {
+        tweet = options.contexts[0].get(propertyPath);
+    }
+    var parsed = twttr.txt.autoLink(tweet);
+    return new Handlebars.SafeString(parsed);
+});
+
+Ember.Handlebars.registerHelper('ago',
+function(propertyName, options) {
+    var timestamp = Ember.get(options.contexts[0]);
+    if (options.hash.isSeconds) {
+        // the given property represents seconds since UNIX epoch, so we multiply
+        // by 1000 to get the date in milliseconds since UNIX epoch
+        timestamp *= 1000;
+    }
+    return moment(new Date(timestamp)).fromNow();
+});
+*/
